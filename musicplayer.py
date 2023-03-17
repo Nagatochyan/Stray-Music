52a81f2b73a48870d01b8c30849064791adb6202
Xx_Uribo_xX <94958239+Nagatochyan@users.noreply.github.com>
2023-03-17 16:45:39 +0900

    Update musicplayer.py

--- musicplayer.py
+++ musicplayer.py
@@ -27,7 +27,7 @@ def whatareyouplaying():
     rpc.update(details="Playing stray music",state=presenceyou,large_image="bg",start=time.time())
 #何の曲が流れるとか、順番とか（製作中）
 sv = tk.StringVar()
-nowpte=tk.Entry(textvariable=sv)
+nowpte=tk.Entry(textvariable=sv,fg="#000000")
 nowpte.configure(state='disabled')#書き込み禁止
 nowpte.place(x=200,y=20,width=300,height=30)
 #参照ボタン関数
@@ -35,7 +35,7 @@ def select():
     global lst
     global filenum
     global file_path
-    idir = 'C:\\Users\\tamat\\3D Objects\\百人一首'
+    idir = 'C:\\'
     file_path = tk.filedialog.askdirectory(initialdir = idir)
     lst=glob.glob(file_path+'\\'+'*.mp3')
     filenum=(sum(os.path.isfile(os.path.join(file_path, name)) for name in os.listdir(file_path)))
@@ -128,11 +128,33 @@ def playth():
 def stopth():
     thread=threading.Thread(target=stop)
     thread.start()
-
 #ファイル参照ボタン
 img=tk.PhotoImage(file="UI/docusign.png")
 big_img=img.zoom(2,2)
 selecmu=tk.Button(image=big_img,command=selectth)
+selecmu.place(x=20,y=200)
+#再生開始ボタン
+imgg=tk.PhotoImage(file="UI/play_icon.png")
+small_img = imgg.subsample(1,1)
+playbu=tk.Button(image=small_img,command=playth)
+playbu.place(x=530,y=10)
+#一時停止ボタン
+imggg=tk.PhotoImage(file="UI/stop_icon.png")
+small_imgg= imggg.subsample(1,1)
+stopbu=tk.Button(image=small_imgg,command=stopth)
+stopbu.place(x=570,y=200)
+#音量調節バー         #左右の端に音量のアイコン付けたい
+vol=tk.Label(text="Vol",fg="black",font=("20"))
+vol.place(x=150,y=215)
+scale1 = tk.Scale( from_=0, to=100, length=200, orient = 'h', command=set_volume1 )
+scale1.place(x=200,y=200,width=150)
+scale1.set( 100 )
+#discord起動してない場合の例外処理
+try:
+    richpresence()
+except Exception as e:
+    pass
+root.mainloop()
 selecmu.place(x=20,y=200)
 #再生開始ボタン
 imgg=tk.PhotoImage(file="UI/play_icon.png")
